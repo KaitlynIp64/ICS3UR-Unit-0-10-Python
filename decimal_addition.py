c8fba771e99f1600fad7735d5f44cadc573be251
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-10-13 02:50:31 +0000

    added code

---
+++ decimal_addition.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Kaitlyn Ip
+# Created on: Oct 2022
+# This program adds two numbers inputted by the user
+
+import math
+
+
+def main():
+    # this function calculates the sum of two numbers inputted by user
+
+    # input
+    number_one = float(input("Enter the first number: "))
+    number_two = float(input("Enter the second number: "))
+
+    # process
+    sum_of_num = number_one + number_two
+
+    # output
+    print("")
+    print("{0:,.1f} + {1:,.1f} = {2:,.1f}.".format(number_one, number_two, sum_of_num))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
